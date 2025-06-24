ee97d9ec9c2440c1873621fb2aba2f6cc7902053
pmcsantana <petersson.mcs@gmail.com>
2025-06-23 23:40:30 -0300

    added gpio and joystick(needs fixing)

---
+++ include/joystick.h
@@ -0,0 +1,34 @@
+#ifndef _HAL_JOYSTICK_H
+#define _HAL_JOYSTICK_H
+
+#include "adc.h"
+#include "gpio.h"
+
+typedef struct
+{
+    ADC_t* adc_x;
+    ADC_t* adc_y;
+    GPIO_t* gpio_z;
+    uint8_t pos_x;
+    uint8_t pos_y;
+    bool state_z;
+}JOYSTICK_t;
+
+typedef struct
+{
+    ADC_CONFIG_t x;
+    ADC_CONFIG_t y;
+    GPIO_CONFIG_t z;
+}JOYSTICK_CONFIG_t;
+
+JOYSTICK_t* JOYSTICK_Init( JOYSTICK_CONFIG_t );
+
+void JOYSTICK_Update( JOYSTICK_t* );
+
+uint8_t JOYSTICK_GetX( JOYSTICK_t* );
+
+uint8_t JOYSTICK_GetY( JOYSTICK_t* );
+
+uint8_t JOYSTICK_GetZ( JOYSTICK_t* );
+
+#endif
--- main.c
+++ main.c
@@ -4,12 +4,34 @@
 #include "pico/stdlib.h"
 #include <include/driver1306.h>
 #include <include/gpio.h>
+#include <include/joystick.h>
 
+/****************************
+*         DEFINES
+****************************/
+#define JOYSTICK_THRESHOLD 20
+
+/****************************
+*         TYPEDEF
+****************************/
+typedef union
+{
+  int x; /* -1, 0 or 1 */
+  int y; /* -1, 0 or 1 */
+}JOYSTICK_POS_t;
+
+/****************************
+*         VARIABLES
+****************************/
 D1306_t * gDisplay;
 bool gStateButtonA;
 bool gStateButtonB;
-int snake = 10;
+JOYSTICK_POS_t gStateJoystick;
+int snake[2] = {10,10};
 
+/****************************
+*         TASKS
+****************************/
 void TASK_Display() 
 {
   D1306_CONFIG_t cfg = {
@@ -29,7 +51,7 @@ void TASK_Display()
 
   while (true) 
   {
-    D1306_DrawPixel( gDisplay , snake , 10 );
+    D1306_DrawPixel( gDisplay , snake[0] , snake[1] );
     D1306_Show( gDisplay );
     vTaskDelay(100);
     D1306_Clear( gDisplay );
@@ -55,23 +77,76 @@ void TASK_Buttons()
   GPIO_t* button_a = GPIO_Init( cfg_button_a );
   GPIO_t* button_b = GPIO_Init( cfg_button_b );
 
-  while(1)
+  while(true)
   {
     gStateButtonA = !GPIO_GetInput( button_a );
     gStateButtonB = !GPIO_GetInput( button_b );
-    if( gStateButtonA )
-    {
-      snake++;
-    }else if( gStateButtonB )
-    {
-      snake--;
-    }
     vTaskDelay(100);
   }
 }
 
+void TASK_Joystick()
+{
 
+  JOYSTICK_CONFIG_t cfg_joystick = {
+    .x.pin = 27,
+    .y.pin = 26,
+    .z.pin = 22,
+    .z.direction = 0,
+    .z.logic = 1,
+    .z.mode = 1
+  };
 
+  JOYSTICK_t* joystick = JOYSTICK_Init( cfg_joystick );
+
+  while(1)
+  {
+    JOYSTICK_Update( joystick );
+    if( joystick->pos_x > 255 - JOYSTICK_THRESHOLD )
+    {
+      gStateJoystick.x = 1;
+    }
+    else if( joystick->pos_x < JOYSTICK_THRESHOLD )
+    {
+      gStateJoystick.x = -1;
+    }
+    else
+    {
+      gStateJoystick.x = 0;
+    }
+
+    if( joystick->pos_y > 255 - JOYSTICK_THRESHOLD )
+    {
+      gStateJoystick.y = 1;
+    }
+    else if( joystick->pos_y < JOYSTICK_THRESHOLD )
+    {
+      gStateJoystick.y = -1;
+    }
+    else
+    {
+      gStateJoystick.y = 0;
+    }
+    printf("x:%d,y:%d\n" , joystick->pos_x,joystick->pos_y );
+    printf("x:%d,y:%d\n" , gStateJoystick.x , gStateJoystick.y );
+
+    vTaskDelay(100);
+  }
+}
+
+void TASK_Game()
+{
+  while(true)
+  {
+    snake[0] += gStateJoystick.x;
+    snake[1] += gStateJoystick.y;
+    vTaskDelay(100);
+  }
+}
+
+/****************************
+*         MAIN
+****************************/
 int main() 
 {
   stdio_init_all();
@@ -79,6 +154,10 @@ int main()
   xTaskCreate( TASK_Display, "TASK_Display", 256, NULL, 1, NULL);
 
   xTaskCreate( TASK_Buttons, "TASK_Buttons", 256, NULL, 1, NULL);
+
+  xTaskCreate( TASK_Joystick, "TASK_Joystick", 256, NULL, 1, NULL);
+
+  xTaskCreate( TASK_Game, "TASK_Game", 256, NULL, 1, NULL);
 
   vTaskStartScheduler();
 
